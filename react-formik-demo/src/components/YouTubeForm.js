47ae41c692149e42cd44f8b6e4921230c7958dbe
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2024-05-08 00:16:55 +0530

    Handling form submission

--- react-formik-demo/src/components/YouTubeForm.js
+++ react-formik-demo/src/components/YouTubeForm.js
@@ -8,14 +8,17 @@ function YouTubeForm() {
         name: '',
         email: '',
         channel: ''
+    },
+    onSubmit: values => {
+      console.log('Form data', values)
     }
   })
 
-  console.log('Form values', formik.values)
+  // console.log('Form values', formik.values)
 
   return (
     <div>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <label htmlFor='name'>Name</label>
             <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
 
@@ -25,7 +28,7 @@ function YouTubeForm() {
             <label htmlFor='channel'>Channel</label>
             <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
 
-            <button>Submit</button>
+            <button type='submit'>Submit</button>
 
         </form>
     </div>
