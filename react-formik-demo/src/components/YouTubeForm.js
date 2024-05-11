a78db0abe324178d275d7ae35c38b0e64448d3a7
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2024-05-11 17:24:47 +0530

    Nested objects

--- react-formik-demo/src/components/YouTubeForm.js
+++ react-formik-demo/src/components/YouTubeForm.js
@@ -8,7 +8,11 @@ const initialValues = {
   email: '',
   channel: '',
   comments: '',
-  address: ''
+  address: '',
+  social: {
+    facebook: '',
+    twitter: ''
+  }
 }
 
 const onSubmit = values => {
@@ -94,6 +98,16 @@ function YouTubeForm() {
                   }
                 }
               </Field>
+            </div>
+
+            <div className='form-control'>
+              <label htmlFor='facebook'>Facebook profile</label>
+              <Field type='text' id='facebook' name='social.facebook'/>
+            </div>
+
+            <div className='form-control'>
+              <label htmlFor='twitter'>Twitter profile</label>
+              <Field type='text' id='twitter' name='social.twitter'/>
             </div>
 
             <button type='submit'>Submit</button>
